521691d4bcbead3a0285dd5c03e04b09167dea77
Alex <alexdusselaar@gmail.com>
2024-06-12 15:58:35 +0200

    Level 1 added

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,11 +2,13 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 const Resources = {
     Alchemist: new ImageSource('images/alchemistwolamp.png'),
-    Letter: new ImageSource('images/letter.png')
+    Letter: new ImageSource('images/letter.png'),
+    LevelOneScreenOne: new ImageSource('images/levelone/sceneone.png')
 }
 const ResourceLoader = new Loader([
     Resources.Alchemist,
-    Resources.Letter
+    Resources.Letter,
+    Resources.LevelOneScreenOne
 ])
 
 export { Resources, ResourceLoader }
