9bcbe2d5c35120a465756faa7886fb261af88d3d
nestor kohnlein <Neskoh27@hotmail.com>
2024-06-17 09:43:03 +0200

    label in dialogue
    put the label in dialogue for practical purposes

--- src/js/dialogue.js
+++ src/js/dialogue.js
@@ -1,6 +1,7 @@
 import { Actor, Engine, Vector, Input } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 import { Label, FontUnit, Font} from "excalibur";
+import { Game } from "./game.js";
 
 export class Dialogue extends Actor {
 
@@ -11,5 +12,18 @@ export class Dialogue extends Actor {
     onInitialize() {
         this.graphics.use(Resources.Dialogue.toSprite())
         this.pos = new Vector(620, 200)
+
+        let label = new Label({
+            text: 'Score: 0',
+            pos: new Vector(100, 100),
+            font: new Font({
+                family: 'impact',
+                size: 24,
+                unit: FontUnit.Px
+            })
+        })
+    
+        this.Game.add(label)
+        label.text = 'Score: 100'
     }
 }
--- src/js/resources.js
+++ src/js/resources.js
@@ -11,9 +11,9 @@ const Resources = {
     Dialogue: new ImageSource('images/dialogue.png'),
     L1Door: new ImageSource('images/levelone/door.png'),
     // music
-    MainThemeMusic: new Sound('music/CLE_4_Main_Theme.mp3'),
-    LevelOneMusic: new Sound('music/CLE_4_Level_1.mp3'),
-    LevelTwoMusic: new Sound('music/CLE_4_Level_2.mp3')
+    // MainThemeMusic: new Sound('music/CLE_4_Main_Theme.mp3'),
+    // LevelOneMusic: new Sound('music/CLE_4_Level_1.mp3'),
+    // LevelTwoMusic: new Sound('music/CLE_4_Level_2.mp3')
 }
 const ResourceLoader = new Loader()
 for (let res of Object.values(Resources)) {
