1ba7f13a79904deb40a113042b1886c2e6ca50ae
JTS-Games <1066300@hr.nl>
2024-06-18 12:31:02 +0200

    Update leveltwo.js

--- src/js/leveltwo.js
+++ src/js/leveltwo.js
@@ -7,22 +7,22 @@ import { Door } from './door.js'
 
 export class LevelTwo extends Scene {
     onInitialize(engine) {
-        const leveltwoscreen = new Actor()
-        leveltwoscreen.pos = new Vector(1200, 360)
-        this.add(leveltwoscreen)
-        leveltwoscreen.graphics.use(Resources.levelTwoPlaceholder.toSprite())
 
-        const invisfloor = new InvisibleFloor()
-        invisfloor.pos = new Vector(1900, 700)
-        this.add(invisfloor)
-
-        const door = new Door()
-        door.pos = new Vector(2720,437)
-        this.add(door)
     }
     onActivate(ctx) {
+        console.log(this.engine)
+        const leveltwoscreen = new Actor()
+        leveltwoscreen.pos = new Vector(1280, 360)
+        leveltwoscreen.graphics.use(Resources.levelTwoPlaceholder.toSprite())
+        this.add(leveltwoscreen)
+
+        const invisfloor = new InvisibleFloor()
+        invisfloor.pos = new Vector(1900, 580)
+        this.add(invisfloor)
+
         const alchemist = new Alchemist()
-        alchemist.pos = new Vector(0, 600)
+        alchemist.pos = new Vector(800, 400)
+        alchemist.graphics.use(Resources.Alchemist.toSprite())
         this.add(alchemist)
     }
 }
